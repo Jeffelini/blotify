b01dc33b1da2e5b4a9b45b9859bf096982bec799
Jeffelini <35406352+Jeffelini@users.noreply.github.com>
2018-10-06 17:59:20 -0400

    Initialized frontend with @vue/cli

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="welcome">
+        <h1>Welcome to Blotify</h1>
+        <h3>Blotify helps to create playlists with explicit songs<br/>
+            replaced with non-explicit versions of the song<br/> where possible.</h3>
+        <button id="sign-in">sign-in</button>
+    </div>
+    
+</template>
+
+<script>
+export default {
+  name: "Home"
+  //   props: {
+  //     msg: String
+  //   }
+};
+</script>
+
+<style style lang="scss" scoped>
+button {
+  align-content: center;
+  text-align: center;
+  font-size: 20px;
+  background-color: green;
+  border: none;
+  color: white;
+  padding: 15px 25px;
+  border-radius: 4px;
+}
+
+button:hover {
+  background-color: darkgreen;
+}
+</style>
